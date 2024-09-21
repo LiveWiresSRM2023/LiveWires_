3e78651d4e1c2b27f440937d9bca547ccf90fa26
blzy03 <blessyrinisha.mohanraj@gmail.com>
2024-09-21 16:45:59 +0530

    fix:slider issue fixed

---
+++ src/data/eventData.jsx
@@ -0,0 +1,21 @@
+import CodeVoy from "../assets/CodeVoy.jpg";
+import FlutterWS from "../assets/FlutterWS.jpg";
+import Iot from "../assets/IOT.jpg";
+import TalCarnival from "../assets/TalCarnival.jpg";
+import TechUtsav from "../assets/TechUtsav.jpg";
+import WebBootcamp from "../assets/WebBootcamp.jpg";
+import Recruit from "../assets/Recruit.jpg";
+import LWReboot from "../assets/LWReboot.jpg";
+import Logo from "../assets/Logo.jpg";
+
+export const eventList = [
+  { id: 1, image: CodeVoy, description: 'Description for CodeVoy' },
+  { id: 2, image: FlutterWS, description: 'Description for Flutter Workshop' },
+  { id: 3, image: Iot, description: 'Description for IoT Workshop' },
+  { id: 4, image: TalCarnival, description: 'Description for Talent Carnival' },
+  { id: 5, image: TechUtsav, description: 'Description for Tech Utsav' },
+  { id: 6, image: WebBootcamp, description: 'Description for Web Bootcamppppppp' },
+  { id: 7, image: Recruit, description: 'Description for Recruitment Drive' },
+  { id: 8, image: LWReboot, description: 'Description for Reboot' },
+  { id: 9, image: Logo, description: 'Description for Logo Showcase' },
+];
